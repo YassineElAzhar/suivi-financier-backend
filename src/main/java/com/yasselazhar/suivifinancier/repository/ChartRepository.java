2af2c466f91a941e76f1df7cab8770a179ec43b3
YassineElAzhar <yassine.elazhar@gmail.com>
2022-09-25 22:49:31 -0400

    Chart - update query to postgresql

--- src/main/java/com/yasselazhar/suivifinancier/repository/ChartRepository.java
+++ src/main/java/com/yasselazhar/suivifinancier/repository/ChartRepository.java
@@ -16,15 +16,27 @@ import com.yasselazhar.suivifinancier.model.Income;
 public interface ChartRepository extends JpaRepository<Income, Integer> {
 
 	//Pour les requête JPQL query nous utilisons les projections (en passant par des interfaces)
-	@Query(value = "SELECT DATE_FORMAT(income.date_income, '%Y-%m') as yearMonth, SUM(income.montant) as sommeMontant FROM income group by DATE_FORMAT(income.date_income, '%Y-%m') ASC HAVING yearMonth = year(curdate())",
+	@Query(value = "SELECT to_char(income.date_income, 'yyyy-mm') as yearMonth, "
+			+ "SUM(income.montant) as sommeMontant, "
+			+ "to_char(income.date_income, 'yyyy') as year "
+			+ "FROM income "
+			+ "group by to_char(income.date_income, 'yyyy-mm'), to_char(income.date_income, 'yyyy') "
+			+ "HAVING to_char(income.date_income, 'yyyy') = to_char(CURRENT_DATE,'yyyy') "
+			+ "ORDER BY to_char(income.date_income, 'yyyy-mm') ASC;",
 			  nativeQuery = true)
 	List<ChartInOut> getChartInOutIncomesCurrentYear();
 	
 
 
 	//Pour les requête JPQL query nous utilisons les projections (en passant par des interfaces)
-	@Query(value = "SELECT DATE_FORMAT(expense.date_expense, '%Y-%m') as yearMonth, SUM(expense.montant) as sommeMontant FROM expense group by DATE_FORMAT(expense.date_expense, '%Y-%m') ASC HAVING yearMonth = year(curdate())",
-			  nativeQuery = true)
+	@Query(value = "SELECT to_char(expense.date_expense, 'yyyy-mm') as yearMonth, "
+			+ "SUM(expense.montant) as sommeMontant, "
+			+ "to_char(expense.date_expense, 'yyyy') as year "
+			+ "FROM expense "
+			+ "group by to_char(expense.date_expense, 'yyyy-mm'), to_char(expense.date_expense, 'yyyy') "
+			+ "HAVING to_char(expense.date_expense, 'yyyy') = to_char(CURRENT_DATE,'yyyy') "
+			+ "ORDER BY to_char(expense.date_expense, 'yyyy-mm') ASC;",
+				nativeQuery = true)
 	List<ChartInOut> getChartInOutExpensesCurrentYear();
 	
 }
